67322021b33f74cb41c101ce9d0ef52e2173905f
efberrio <fabio.berrio_bisa@outlook.com>
2019-02-02 10:20:03 -0500

    print

--- EdgeDetectionCuda.cu
+++ EdgeDetectionCuda.cu
@@ -47,7 +47,7 @@ __global__ void edgeDetectionCuda (int *pixels, int *tempImage, int width, int h
 	Avoid accesing data beyond the end of the arrays */
 	if (index < imageSize) {
 		if (index < 5000) {
-			printf("entrando a cuda\n");
+			printf("entrando a cuda ancho=%d, alto=%d\n", width, height);
 		}
 		x = index % width;
 
