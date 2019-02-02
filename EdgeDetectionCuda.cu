768e91e9e4e00b226e250203904f16ac33331512
efberrio <fabio.berrio_bisa@outlook.com>
2019-02-02 11:26:12 -0500

    print

--- EdgeDetectionCuda.cu
+++ EdgeDetectionCuda.cu
@@ -507,12 +507,12 @@ void Image::scaleImage(){
 
 	findMax();
 
-	print("OK1\N");
+	printf("OK1\N");
 
 	int *d_pixels;
 	size_t size = imageSize * sizeof(int);
     cudaError_t err = cudaSuccess;
-	print("OK2\N");
+	printf("OK2\N");
 
 	/* Allocate memory in device */
 	err = cudaMalloc((void **) &d_pixels, size);
@@ -520,7 +520,7 @@ void Image::scaleImage(){
         fprintf(stderr, "Failed to allocate device vector pixels (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-	print("OK3\N");
+	printf("OK3\N");
 
 	/* Copy data to device */
 	err = cudaMemcpy(d_pixels, pixels, size, cudaMemcpyHostToDevice);
@@ -528,7 +528,7 @@ void Image::scaleImage(){
         fprintf(stderr, "Failed to copy vector pixels from host to device (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-	print("OK4\N");
+	printf("OK4\N");
 
 	/* Launch scaleImageCuda() kernel on device with N threads in N blocks */
 	scaleImageCuda<<<(imageSize + (THREADS_PER_BLOCK - 1)) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_pixels, minpix, maxpix, imageSize);
@@ -537,7 +537,7 @@ void Image::scaleImage(){
         fprintf(stderr, "Failed to launch scaleImageCuda kernel (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-	print("OK5\N");
+	printf("OK5\N");
 
 	/* Copy data to tohost device */
 	err = cudaMemcpy(pixels, d_pixels, size, cudaMemcpyDeviceToHost);
@@ -545,7 +545,7 @@ void Image::scaleImage(){
         fprintf(stderr, "Failed to copy vector pixels from device to host (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-	print("OK6\N");
+	printf("OK6\N");
 
 	/* Clean-up */
 	err = cudaFree(d_pixels);
@@ -553,14 +553,14 @@ void Image::scaleImage(){
         fprintf(stderr, "Failed to free device vector pixels (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-	print("OK7\N");
+	printf("OK7\N");
 
     err = cudaDeviceReset();
     if (err != cudaSuccess){
         fprintf(stderr, "Failed to deinitialize the device! error=%s\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-	print("OK8\N");
+	printf("OK8\N");
 
 	maxPixelValue = 255;
 
