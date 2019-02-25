a6f22216fb93e4554e5105de15553a021d3e0a82
efberrio <fabio.berrio_bisa@outlook.com>
2019-02-25 10:29:36 -0500

    cuda with variable threads

---
+++ cuda/edgeDetectionCudaVariableThreads.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "EDGE DETECTION - CUDA - VARIABLE THREADS - START"
+echo "run;image;time" > results_cuda.csv
+for (( i=1; i<=5; i++ ));
+do
+	for (( j=1; j<=3; j=j+1 ));
+	do
+		for (( k=32; k<=1024; k=k*2 ));
+		do
+			output=$(eval "time ./EdgeDetectionCudaVariableThreads image_"${j}".pgm image_"${j}"_out_cuda.pgm "${k}"" 2>&1)
+			realTime=${output:8:5}
+			echo ""$i";"$j";"$output >> results_cuda_vt.csv
+		done
+	done
+done
+echo "EDGE DETECTION - CUDA - VARIABLE THREADS - END"
